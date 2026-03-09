b6bae7cb7c9e1ee89b979555b854ab62e1c7e4d5
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-03-09 09:24:20 -0500

    pilas

---
+++ pilasclase/pila.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class pila{
+    public static void main(String[] args) {
+        boolean seguir = true;
+        Stack<Integer> p = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        while(seguir){
+            p.push((int)(Math.random()*50+1));
+            System.out.println("Desea continuar 1 = s1, 2 = no");
+            if (sc.nextInt() == 2){
+            seguir = false;
+            }
+        } 
+       
+        System.out .println(p); 
+    }
+}
