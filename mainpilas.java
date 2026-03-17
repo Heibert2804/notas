0f3504aded3943953caac10b8ed9373ac302be1f
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-03-17 08:37:59 -0500

    pilas

--- mainpilas.java
+++ mainpilas.java
@@ -3,9 +3,10 @@ import java.util.Stack;
 public class mainpilas {
     public static void main(String[] args) {
         boolean seguir = true;
-        pilasnuevas u = new pilasnuevas();
+        pilasnuevas m = new pilasnuevas();
         Scanner sc = new Scanner(System.in);
         Stack<Integer> pila = new Stack<>();
+        int numero = 0;
         while(seguir){
             System.out.println("Bienvenido miremos un CRUD");
             System.out.println("Escoja una opcion");
@@ -14,26 +15,46 @@ public class mainpilas {
             System.out.println("3. Modificar");
             System.out.println("4. Eliminar de la existencia");
             System.out.println("5. Salir");
-            int opt = u.ValidarEntero(sc);
+            int opt = m.ValidarEntero(sc);
             System.out.println(opt);
             switch (opt) {
                 case 1:
-                    pila = u.LlenarPila(pila);
+                    pila = m.LlenarPila(pila);
                     break;
                 case 2:
-                    u.MostrarPila(pila);
+                    m.MostrarPila(pila);
                     break;
                 case 3:
-                    
+                    numero = m.Pedirdato(1);
+                    pila = m.ModificarPila(pila, numero);
                     break;
                 case 4:
-                    
+                    int choise = 0;
+                    numero = m.Pedirdato(4);
+                    System.out
+                            .println("que metodo desea implementar 1) el depilas, 2) el listas , 3) funcion anonima ");
+                    choise = sc.nextInt();
+                    switch (choise) {
+                        case 1:
+                            pila = m.Eliminar(pila, numero);
+                            break;
+                        case 2:
+                            pila = m.Eliminar1(pila, numero);
+                            break;
+
+                        default:
+                            pila = m.Eliminar2(pila, numero);
+                            break;
+                    }
+
                     break;
                 case 5:
-                    
+                    System.out.println("Gracias por venir ");
+                    seguir = false;
                     break;
-            
+
                 default:
+                    System.out.println("home de 1 a 5 no joda");
                     break;
             }
         }
--- pila.java
+++ pila.java
@@ -1,20 +1,20 @@
 
-import java.util.Stack;
-import java.util.Scanner;
+// import java.util.Stack;
+// import java.util.Scanner;
 
-public class pila{
-    public static void main(String[] args) {
-        boolean seguir = true;
-        Stack<Integer> p = new Stack<>();
-        Scanner sc = new Scanner(System.in);
-        while(seguir){
-            p.push((int)(Math.random()*50+1));
-            System.out.println("Desea continuar 1 = s1, 2 = no");
-            if (sc.nextInt() == 2){
-            seguir = false;
-            }
-        } 
+// public class pila{
+//     public static void main(String[] args) {
+//         boolean seguir = true;
+//         Stack<Integer> p = new Stack<>();
+//         Scanner sc = new Scanner(System.in);
+//         while(seguir){
+//             p.push((int)(Math.random()*50+1));
+//             System.out.println("Desea continuar 1 = s1, 2 = no");
+//             if (sc.nextInt() == 2){
+//             seguir = false;
+//             }
+//         } 
        
-        System.out .println(p); 
-    }
-}
+//         System.out .println(p); 
+//     }
+// }
